5764996f130b31c5fea27f537d1f8f78b1855f35
Alexey Shalin <shalin.2001@bk.ru>
2020-11-24 21:16:01 +0300

    Rollback craco config file

--- modern/craco.config.js
+++ modern/craco.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  eslint: {
-    enable: true,
-    mode: "extends" || "file",
-    configure: (eslintConfig) => { return eslintConfig; },
-    loaderOptions: (eslintOptions) => { return eslintOptions; }
-  },
   webpack: {
-    configure: (webpackConfig) => { return webpackConfig; }
+    configure: webpackConfig => {
+      const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+        ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
+      );
+
+      webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+      return webpackConfig;
+    }
   }
 };
